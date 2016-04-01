c6cefda4278504cecdb619fc6c3be0e24083070b
Roland Fung <roland.c.fung@gmail.com>
2016-04-01 01:18:48 -0700

    fix server config

--- server-config.js
+++ server-config.js
@@ -19,7 +19,7 @@ app.configure(function() {
 app.get('/', util.checkUser, handler.renderIndex);
 app.get('/create', util.checkUser, handler.renderIndex);
 
-app.get('/links', util.checkUser, handler.fetchLinks, handler.renderIndex);
+app.get('/links', util.checkUser, handler.fetchLinks);
 app.post('/links', handler.saveLink);
 
 app.get('/login', handler.loginUserForm);
